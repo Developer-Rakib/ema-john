3207c39d4388036c6e589d02df29a32d6e6c35d1
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-03-22 16:02:29 +0600

    bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import Nav from './components/nav/Nav';
 import Shop from './components/shop/Shop';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
