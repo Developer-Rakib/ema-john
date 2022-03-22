86e6f937d7c22763563875b7510ec2c773f7a211
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-03-22 16:12:47 +0600

    done

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { useState } from 'react'
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react'
 import Product from '../product/Product';
 import './Shop.css';
 
